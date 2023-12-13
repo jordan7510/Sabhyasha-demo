f3d8150b199bcef6c1bc4589ca7262503cfa4a5e
souravbsk <souravbsk01@gmail.com>
2023-12-13 16:06:25 +0600

    gupta code

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeroSection from './HeroSection/HeroSection';
 import SolveSection from './SolveSection/SolveSection';
+import Testimonial from './Testimonial/Testimonial';
 
 const Home = () => {
     return (
         <div>
            <HeroSection></HeroSection>
+           <Testimonial></Testimonial>
            <SolveSection></SolveSection>
         </div>
     );
---
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Testimonial = () => {
+    return (
+        <div className='py-24 bg-testimonial'>
+            <h1>hello world</h1>
+        </div>
+    );
+};
+
+export default Testimonial;
